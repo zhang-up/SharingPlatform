2a19b70a750d44bf36627a5e74d0c67d0841f2e7
zhang-up <793181667@qq.com>
2019-09-12 17:50:03 +0800

    d

--- SharingPlatform/src/main/java/com/project/po/TestPO.java
+++ SharingPlatform/src/main/java/com/project/po/TestPO.java
@@ -1,20 +1,11 @@
 package com.project.po;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class TestPO {
 	private int id;
 	private String name;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-
 }
