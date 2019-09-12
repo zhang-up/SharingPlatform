e81dc36788aa5cc3202d824684d8bae7394b559a
xyx <xyogu@qq.com>
2019-09-12 17:45:25 +0800

    添加工具类

---
+++ SharingPlatform/src/main/java/com/project/utils/UUIDUtil.java
@@ -0,0 +1,31 @@
+package com.project.utils;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+	/**
+	 * 带‘-’的UUID
+	 * @return
+	 */
+	public static String getUUID(){
+		UUID uuid = UUID.randomUUID();
+		return uuid.toString().toUpperCase();
+	}
+	
+	/**
+	 * 32位UUID
+	 * @return
+	 */
+	public static String getUUID32(){
+		UUID uuid = UUID.randomUUID();
+		return uuid.toString().replace("-", "").toUpperCase();
+	}
+	public static void main(String[] args) {
+		
+		for(int i=0 ; i < 30 ; i++){
+			System.out.println(UUIDUtil.getUUID32());
+		}
+		
+	}
+}
